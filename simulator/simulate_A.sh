c6849c3381007a9f39852e4ee505668c6483cab7
jh989876525 <james_chen@innodisk.com>
2026-03-09 10:10:29 +0800

    feat: random rw size control

--- simulator/simulate_A.sh
+++ simulator/simulate_A.sh
@@ -4,7 +4,7 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-TARGET="/dev/sde"
+TARGET="/dev/sdx"
 CONFIG_FILE="fio_behavior.cfg"
 ROUND_TIME=3600
 FIO_CMD="fio $CONFIG_FILE --filename=$TARGET --runtime=$ROUND_TIME"
@@ -18,31 +18,36 @@ while true; do
     CURRENT_HOUR=$(date +%H)
     CHANCE=$(( $RANDOM % 100 ))
 
+    SIZE_K=$(( $RANDOM % 100))
+    SIZE_M=$(( $RANDOM % 100))
+    SIZE_G1=$(( $RANDOM % 3))
+    SIZE_G2=$(( $RANDOM % 100))
+
     if [ "$CURRENT_HOUR" -ge 9 ] && [ "$CURRENT_HOUR" -lt 18 ]; then
         echo "核心工作時間 ($CURRENT_HOUR:00)"
 
         if [ $CHANCE -lt 80 ]; then
             echo ">> 觸發: 日常辦公 (80% 機率)"
-            $FIO_CMD --section=daily_office > /dev/null
+            $FIO_CMD --section=daily_office --size=${SIZE_M}M > /dev/null
         elif [ $CHANCE -lt 90 ]; then
             echo ">> 觸發: 系統更新/安裝 (10% 機率)"
-            $FIO_CMD --section=software_update > /dev/null
+            $FIO_CMD --section=software_update --size=${SIZE_G1}G > /dev/null
         elif [ $CHANCE -lt 95 ]; then
             echo ">> 警告: 模擬惡意檔案外傳 (5% 機率)"
-            $FIO_CMD --section=malicious_copy > /dev/null
+            $FIO_CMD --section=malicious_copy --size=${SIZE_G2}G > /dev/null
         else
             echo ">> 警告: 模擬勒索軟體加密 (5% 機率)"
-            $FIO_CMD --section=ransomware > /dev/null
+            $FIO_CMD --section=ransomware --size=${SIZE_G2}G > /dev/null
         fi
     else
         echo "非工作時間 ($CURRENT_HOUR:00)"
 
         if [ $CHANCE -lt 90 ]; then
             echo ">> 觸發: 系統閒置 (90% 機率)"
-            $FIO_CMD --section=idle > /dev/null
+            $FIO_CMD --section=idle --size=${SIZE_K}K > /dev/null
         else
             echo ">> 觸發: 離峰時段系統自動更新 (10% 機率)"
-            $FIO_CMD --section=software_update > /dev/null
+            $FIO_CMD --section=software_update --size=${SIZE_G1}G > /dev/null
         fi
     fi
 
